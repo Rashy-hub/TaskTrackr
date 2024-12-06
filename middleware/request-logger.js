6480f8c8ffaeac5f22c436f077ac18892a509841
Rashy-hub <yagoubi.r@gmail.com>
2024-12-06 16:49:25 +0100

    fix : useless logs deleted

--- middleware/request-logger.js
+++ middleware/request-logger.js
@@ -4,13 +4,11 @@ function logRequest(req, res, next) {
     const startTime = Date.now()
 
     logger.info(`Incoming request: ${method} ${url}`)
-    logger.info(`Incomming Session info ${JSON.stringify(req.session)}`)
 
     res.on('finish', () => {
         const statusCode = res.statusCode
         const elapsedTime = Date.now() - startTime
         logger.info(`Request completed: ${method} ${url} - Status: ${statusCode} - Duration: ${elapsedTime}ms`)
-        logger.info(`Request send new Session info ${JSON.stringify(req.session)}`)
     })
 
     next()
